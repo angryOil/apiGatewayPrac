00c1363e1faefa3387cd93599906624e0041aeb5
JOY <103836369+angryOil@users.noreply.github.com>
2023-11-27 16:15:26 +0900

    cafe 카페리스트 조회 확인

--- internal/service/cafe/service.go
+++ internal/service/cafe/service.go
@@ -5,6 +5,8 @@ import (
 	req2 "apiGateway/internal/cli/cafe/req"
 	"apiGateway/internal/controller/cafe/req"
 	cafe2 "apiGateway/internal/domain/cafe"
+	page2 "apiGateway/internal/page"
+	"apiGateway/internal/service/cafe/res"
 	"context"
 )
 
@@ -31,4 +33,20 @@ func (s Service) Create(ctx context.Context, c req.CreateCafeDto) error {
 		Description: description,
 	})
 	return err
+}
+
+func (s Service) GetList(ctx context.Context, reqPage page2.ReqPage) ([]res.GetCafes, int, error) {
+	domains, cnt, err := s.r.GetList(ctx, reqPage)
+	if err != nil {
+		return []res.GetCafes{}, 0, err
+	}
+	dto := make([]res.GetCafes, len(domains))
+	for i, d := range domains {
+		v := d.ToCafeListInfo()
+		dto[i] = res.GetCafes{
+			Id:   v.Id,
+			Name: v.Name,
+		}
+	}
+	return dto, cnt, nil
 }
